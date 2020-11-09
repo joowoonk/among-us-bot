52794bafb876c0556042c8f0f82c30326de7f5b8
Your Name <joowoonk@gmail.com>
2020-11-09 13:00:38 -0800

    found typo

--- commands/dance.js
+++ commands/dance.js
@@ -1,6 +1,6 @@
 
 module.exports.run = async (bot, message, args) => {
-    message.channel.send(`sHaKe tHaT B00TY- ${ars[0]}`)
+    message.channel.send(`sHaKe tHaT B00TY- ${args[0]}`)
     return message.channel.send(("dance time", {files: ["https://i.kym-cdn.com/photos/images/original/001/893/924/569.gif"]}))
 }
 
