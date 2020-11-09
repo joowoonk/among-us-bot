5b3136d5f4f4393bbf613a47e95e0b2b1db0d3ba
Your Name <joowoonk@gmail.com>
2020-11-09 12:52:57 -0800

    dance

--- commands/dance.js
+++ commands/dance.js
@@ -1,12 +1,9 @@
 
 module.exports.run = async (bot, message, args) => {
-    // let picture = message.author.avatarURL;
-
-    // return message.channel.send({file: {
-    //     attachment: meeting,
-    //     name:"meeting"
-    // }})
-    message.channel.send(`move that BOOTY`)
+    if (args.length === 0){
+        return;
+    }
+    message.channel.send(`sHaKe tHaT B00TY, ${ars[0]}`)
     return message.channel.send(("dance time", {files: ["https://i.kym-cdn.com/photos/images/original/001/893/924/569.gif"]}))
 }
 
