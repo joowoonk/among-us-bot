52389e74163000da5acf6444163ce1f0d92ed546
Your Name <joowoonk@gmail.com>
2020-11-09 01:47:15 -0800

    update

--- commands/streaming.js
+++ commands/streaming.js
@@ -2,7 +2,7 @@ const discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 
-    return message.channel.send("Hello Amongers! check out => https://www.twitch.tv/lizzyliesalot")
+    return message.channel.send("Hello Amongers! We are streaming right now! => https://www.twitch.tv/lizzyliesalot")
 }
 
 module.exports.help ={
