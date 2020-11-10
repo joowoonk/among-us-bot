26a181a1928453fc62fa8937820a6c2b4562c97a
Your Name <joowoonk@gmail.com>
2020-11-10 09:50:22 -0800

    color

---
+++ commands/color.js
@@ -0,0 +1,12 @@
+let color = ["dark green","black","purple","blue","light green","white","pink","mint","yellow",'red',"brown","orange"]
+// console.log(color.length)
+module.exports.run = async (bot, message, args) => {
+    if (args.length === 1){
+        return;
+    }
+    return message.channel.send(`I am now sentient, trust me, it's ${color[Math.floor(Math.random() * 13)]}!!! 😲😲😲`)
+}
+
+module.exports.help ={
+    name: "color"
+}
