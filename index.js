784805ff0d4778131d5df03c8f4a14c1424365b6
Your Name <joowoonk@gmail.com>
2020-11-26 13:55:08 -0800

    add

--- index.js
+++ index.js
@@ -47,7 +47,39 @@ bot.on("message", (message, args) => {
     return message.channel.send(`Hello, <@${message.member.id}>, I'm your friendly bot!`);
 
     // Gute Nacht, dein freundlicher Bot wird dich vermissen
-   } else if (message.content.includes("night")) {
+   } else if (message.content.includes("morten")) {
+    return message.channel.send(`Morning <@${message.member.id}>!`);
+
+    
+   }else if (message.content.includes("L0cina")) {
+    return message.channel.send(`Hello L0Cina`);
+
+    
+   }else if (message.content.includes("Liesa")) {
+    return message.channel.send(`Hello Liesa`);
+
+    
+   }else if (message.content.includes("Vivu")) {
+    return message.channel.send(`Hello Vivu`);
+
+    
+   }else if (message.content.includes("Abi")) {
+    return message.channel.send(`Hello Abi`);
+
+    
+   }else if (message.content.includes("Nick")) {
+    return message.channel.send(`Hello Nick`);
+
+    
+   }else if (message.content.includes("Jenny")) {
+    return message.channel.send(`Hello Jenny`);
+
+    
+   }else if (message.content.includes("Joowoon")) {
+    return message.channel.send(`Hello?`);
+
+    
+   }else if (message.content.includes("night")) {
     return message.channel.send(`Gute Nacht, dein freundlicher Bot wird dich vermissen <@${message.member.id}>!`);
 
     
