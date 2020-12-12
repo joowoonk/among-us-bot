098ff06e7fd83f3fc928c804d3fc716f2fb93785
Your Name <joowoonk@gmail.com>
2020-12-11 20:08:54 -0800

    updated eject, removed all the spacings

--- commands/eject.js
+++ commands/eject.js
@@ -3,12 +3,13 @@ let eject = ["an imposter","not an imposter"]
 // console.log(eject.length)
 module.exports.run = async (bot, message, args) => {
 
-    return message.channel.send(    `.      　。　　　　•　    　ﾟ　　。\n
-    　　.　　　.　　　  　　.　　　　　。　　   。　.\n
-     　.　　      。　        ඞ   。　    .    •\n
-     •        ${args[0]} was ${eject[Math.floor(Math.random() * 2)]}。。。　.\n
-    　 　　。　　　　　　ﾟ　　　.　　　　　.\n
-    ,　　　　.　 .　　       .               。\n`)
+    return message.channel.send(
+    `.      　。　　　　•　    　ﾟ　　。
+    　　.　　　.　　　  　　.　　　　　。　　   。　.
+     　.　　      。　        ඞ   。　    .    •
+     •        ${args[0]} was ${eject[Math.floor(Math.random() * 2)]}。。。
+    　 　　。　　　　　　ﾟ　　　.　　　　　.
+    ,　　　　.　 .　　       .               。`)
 }
 
 module.exports.help ={
