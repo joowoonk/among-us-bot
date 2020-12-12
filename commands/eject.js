d8d00757d697b04bc2007b353684e00f3f183d01
Your Name <joowoonk@gmail.com>
2020-12-11 19:54:48 -0800

    updated eject

--- commands/eject.js
+++ commands/eject.js
@@ -2,8 +2,13 @@ let eject = ["an imposter","not an imposter"]
 // console.log(eject[Math.floor(Math.random() * 2)])
 // console.log(eject.length)
 module.exports.run = async (bot, message, args) => {
-    
-    return message.channel.send(`${args[0]} was ${eject[Math.floor(Math.random() * 2)]}`)
+
+    return message.channel.send(    `.      　。　　　　•　    　ﾟ　　。\n
+    　　.　　　.　　　  　　.　　　　　。　　   。　.\n
+     　.　　      。　        ඞ   。　    .    •\n
+     •        ${args[0]} was ${eject[Math.floor(Math.random() * 2)]}。。。　.\n
+    　 　　。　　　　　　ﾟ　　　.　　　　　.\n
+    ,　　　　.　 .　　       .               。\n`)
 }
 
 module.exports.help ={
