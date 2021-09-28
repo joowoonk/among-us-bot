ee0f26847165ad2eb14ba2248ff2ee8026913d62
Joo Woon Kang <49606018+joowoonk@users.noreply.github.com>
2021-09-28 02:17:55 -0700

    added another gif

--- commands/lisa.js
+++ commands/lisa.js
@@ -393,6 +393,7 @@ let blackpink = [
   "https://tenor.com/view/blackpink-lisa-lilifilm-the-movie-dance-gif-20356561",
   "https://tenor.com/view/blackpink-lisa-lilifilm-the-movie-dance-gif-20356557",
   "https://tenor.com/view/lisa-gif-lili-gif-19118592",
+  "https://tenor.com/view/blackpink-dancing-hot-dance-sexy-gif-18965494",
   "https://tenor.com/view/blackpink-dancing-lisa-lilifilm-the-gif-20354492",
   "https://tenor.com/view/blackpink-synchronized-lisa-lilifilm-the-gif-20354489",
   "https://tenor.com/view/blackpink-dancing-lisa-lilifilm-the-gif-20354490",
