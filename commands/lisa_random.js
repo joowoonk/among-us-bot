d641ec3878fa0670c4c7af4d8adf71c5e2cf145b
Joo Woon Kang <49606018+joowoonk@users.noreply.github.com>
2021-10-04 09:59:24 -0700

    lisa random added

---
+++ commands/lisa_random.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+
+
+// axios
+//   .get("https://g.tenor.com/v1/random?q=lisa%blackpink&key=LIVDSRZULELA")
+//   // https://g.tenor.com/v1/random?lisarandom&key=LIVDSRZULELA&limit=1
+//   .then((res) => {
+//     console.log(
+//       res.data.results[Math.floor(Math.random() * res.data.results.length - 1)]
+//         .itemurl
+//     );
+//     return message.channel.send(res);
+//   })
+//   .catch((er) => console.error(er));
+module.exports.run = async (bot, message, args) => {
+  axios
+    .get("https://g.tenor.com/v1/random?q=lisa%blackpink&key=LIVDSRZULELA")
+    // https://g.tenor.com/v1/random?lisarandom&key=LIVDSRZULELA&limit=1
+    .then((res) => {
+      return message.channel.send(
+        res.data.results[
+          Math.floor(Math.random() * res.data.results.length - 1)
+        ].itemurl
+      );
+    })
+    .catch((er) => console.error(er));
+};
+module.exports.help = {
+  name: "lisarandom",
+};
