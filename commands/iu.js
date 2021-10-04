78e52cac319ccb3e496e1b6d8560bfffaf6de450
Joo Woon Kang <49606018+joowoonk@users.noreply.github.com>
2021-10-04 09:32:25 -0700

    added API Key

--- commands/iu.js
+++ commands/iu.js
@@ -5,9 +5,12 @@ const axios  = require("axios");
 // console.log(iu[x])
 
 module.exports.run = async (bot, message, args) => {
-  axios.get("https://g.tenor.com/v1/random?iu").then(res=> {
-      return message.channel.send(res)
-  }).catch(er => console.error(er));
+  axios
+    .get("https://g.tenor.com/v1/random?iu" + process.env.APIKEY)
+    .then((res) => {
+      return message.channel.send(res);
+    })
+    .catch((er) => console.error(er));
   
 };
 
