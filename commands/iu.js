bf1e58f1aabda412747e6e2bfac8930a11353136
Joo Woon Kang <49606018+joowoonk@users.noreply.github.com>
2021-10-04 09:38:17 -0700

    fixed url

--- commands/iu.js
+++ commands/iu.js
@@ -2,11 +2,12 @@ const axios  = require("axios");
 
 
 
-// console.log(iu[x])
+console.log(process.env.APIKEY)
 
 module.exports.run = async (bot, message, args) => {
   axios
-    .get("https://g.tenor.com/v1/random?iu" + process.env.APIKEY)
+    .get("g.tenor.com/v1/random?iu&key=" + process.env.APIKEY+"&limit=1")
+    // https://g.tenor.com/v1/random?iu&key=LIVDSRZULELA&limit=1
     .then((res) => {
       return message.channel.send(res);
     })
