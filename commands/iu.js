758955bb61d2dcb40677044b1b44c5f14b4d6278
Joo Woon Kang <49606018+joowoonk@users.noreply.github.com>
2021-10-04 07:28:17 -0700

    added iu using tenor api

---
+++ commands/iu.js
@@ -0,0 +1,16 @@
+const { axios } = require("axios");
+
+
+
+// console.log(iu[x])
+
+module.exports.run = async (bot, message, args) => {
+  axios.get("https://g.tenor.com/v1/random?iu").then(res=> {
+      return message.channel.send(res)
+  }).catch(er => console.error(er));
+  
+};
+
+module.exports.help = {
+  name: "iu",
+};
