6865a0e283cfbfa224bd6322ba88ad39b06cde21
Joo Woon Kang <49606018+joowoonk@users.noreply.github.com>
2021-10-04 09:53:14 -0700

    added enponting with right res direcotry

--- commands/iu.js
+++ commands/iu.js
@@ -3,18 +3,31 @@ const axios  = require("axios");
 
 
 console.log(process.env.APIKEY)
-
+axios
+  .get("https://g.tenor.com/v1/random?q=iu&key=LIVDSRZULELA")
+  // https://g.tenor.com/v1/random?iu&key=LIVDSRZULELA&limit=1
+  .then((res) => {
+ 
+    console.log(
+      res.data.results[Math.floor(Math.random() * res.data.results.length - 1)]
+        .itemurl
+    );
+    return message.channel.send(res);
+  })
+  .catch((er) => console.error(er));
 module.exports.run = async (bot, message, args) => {
   axios
-    .get("g.tenor.com/v1/random?iu&key=" + process.env.APIKEY+"&limit=1")
+    .get("https://g.tenor.com/v1/random?q=iu&key=LIVDSRZULELA")
     // https://g.tenor.com/v1/random?iu&key=LIVDSRZULELA&limit=1
     .then((res) => {
-      return message.channel.send(res);
-    })
-    .catch((er) => console.error(er));
   
-};
-
+      return message.channel.send(
+        res.data.results[
+          Math.floor(Math.random() * res.data.results.length - 1)
+        ].itemurl
+      )})
+    .catch((er) => console.error(er)) 
+    };
 module.exports.help = {
   name: "iu",
 };
