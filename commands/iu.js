bb4c2d2a2199300aea4cbf32a90abf4ce65e68e3
Joo Woon Kang <49606018+joowoonk@users.noreply.github.com>
2021-10-05 09:56:19 -0700

    debugging

--- commands/iu.js
+++ commands/iu.js
@@ -2,19 +2,7 @@ const axios  = require("axios");
 
 
 
-console.log(process.env.APIKEY)
-axios
-  .get("https://g.tenor.com/v1/random?q=iu&key=LIVDSRZULELA")
-  // https://g.tenor.com/v1/random?iu&key=LIVDSRZULELA&limit=1
-  .then((res) => {
- 
-    console.log(
-      res.data.results[Math.floor(Math.random() * res.data.results.length - 1)]
-        .itemurl
-    );
-    return message.channel.send(res);
-  })
-  .catch((er) => console.error(er));
+
 module.exports.run = async (bot, message, args) => {
   axios
     .get("https://g.tenor.com/v1/random?q=iu&key=LIVDSRZULELA")
