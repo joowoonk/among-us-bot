a6603bb5cffff307a9eb56bd67778ed8e2bc41bc
Joo Woon Kang <49606018+joowoonk@users.noreply.github.com>
2021-10-05 09:53:34 -0700

    added bunch of people

---
+++ commands/nct.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+
+module.exports.run = async (bot, message, args) => {
+  axios
+    .get("https://g.tenor.com/v1/random?q=nct&key=LIVDSRZULELA")
+    // https://g.tenor.com/v1/random?iu&key=LIVDSRZULELA&limit=1
+    .then((res) => {
+      return message.channel.send(
+        res.data.results[
+          Math.floor(Math.random() * res.data.results.length - 1)
+        ].itemurl
+      );
+    })
+    .catch((er) => console.error(er));
+};
+module.exports.help = {
+  name: "nct",
+};
