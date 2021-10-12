faab0eb3ee42fc9dc346287e140b0648c82f8009
Joo Woon Kang <49606018+joowoonk@users.noreply.github.com>
2021-10-11 20:26:42 -0700

    found the bug in momo, length was calcualted wrong

--- commands/momo.js
+++ commands/momo.js
@@ -1,17 +1,13 @@
 const axios = require("axios");
+let momo = ["momo%twice", "momo%dance", "momo%cute"]
+console.log(momo[Math.floor(Math.random() * momo.length)]);
 
 module.exports.run = async (bot, message, args) => {
-  let momo = ["momo%twice", "momo%dance", "momo%cute"]
-
   axios
     .get("https://g.tenor.com/v1/random?q="+ momo[Math.floor(Math.random() * momo.length - 1)] +"&key=LIVDSRZULELA")
     // https://g.tenor.com/v1/random?momo&key=LIVDSRZULELA&limit=1
     .then((res) => {
-      console.log(
-        res.data.results[
-          Math.floor(Math.random() * res.data.results.length - 1)
-        ]
-      );
+  
       return message.channel.send(
         res.data.results[
           Math.floor(Math.random() * res.data.results.length - 1)
