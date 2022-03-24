22f81af0c9abfc8a8070027b5791b2940210d042
Joo Woon Kang <49606018+joowoonk@users.noreply.github.com>
2022-03-24 00:39:32 -0500

    trying to find a fix

--- commands/help.js
+++ commands/help.js
@@ -5,47 +5,7 @@ module.exports.run = async (bot, message, args) => {
     return;
   }
   return message.channel.send(
-    `
-    Commad Lines
-    $bts
-    $changwook
-    $dahyun
-    $dave
-    $eunwoo
-    $exo
-    $got7
-    $hyunjin
-    $hyunjin
-    $jackson
-    $jangkiyon
-    $jichang
-    $jihyo
-    $jisoo
-    $lalisa
-    $lisa
-    $jisoo
-    $macho
-    $mark
-    $mina
-    $momo
-    $nct
-    $ryujin
-    $seungmin
-    $straykids
-    $tablo
-    $tzuyu
-    $wonwoo
-
-    $color 
-    $dance username
-    $eject username
-    $meeting
-    $greetings
-    $kill username
-    $love username
-    $sus username
-    $vent username
-  `
+    "Commad Lines: $bts $changwook $dahyun $dave $eunwoo $exo $got7 $hyunjin $hyunjin $jackson $jangkiyon $jichang $jihyo $jisoo $lalisa $lisa $jisoo $macho $mark $mina $momo $nct $ryujin $seungmin $straykids $tablo $tzuyu $wonwoo $color $dance username $eject username $meeting $greetings $kill username $love username $sus username $vent username"
   );
 };
 
