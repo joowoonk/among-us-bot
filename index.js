9a82dc0dccc732e7191948729c7e208a352591eb
Joo Woon Kang <49606018+joowoonk@users.noreply.github.com>
2022-03-28 11:40:42 -0500

    add

--- index.js
+++ index.js
@@ -247,24 +247,24 @@ bot.on("message", (message, args) => {
 //     serverQueue.textChannel.send(`Start playing: **${song.title}**`);
 //   }
 
-// bot.on("message", async message => {
-//     if (message.author.bot) return;
-//     if (message.channel.type === "dm") return
+bot.on("message", async message => {
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return
 
-//     let prefix = config.prefix
-//     let messageArray = message.content.split(" ");
-//     let command = messageArray[0].toLowerCase()
-//     let args = messageArray.slice(1)
+    let prefix = config.prefix
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0].toLowerCase()
+    let args = messageArray.slice(1)
 
-//     if(!command.startsWith(prefix)) return;
+    if(!command.startsWith(prefix)) return;
 
 
 
     
 
-//     let cmd = bot.commands.get(command.slice(prefix.length));
-//     if (cmd) cmd.run(bot, message, args)
-// })
+    let cmd = bot.commands.get(command.slice(prefix.length));
+    if (cmd) cmd.run(bot, message, args)
+})
 
 
 
