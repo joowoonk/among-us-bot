9b98703764a4f3284279445ae756ec6dbb21bd18
Joo Woon Kang <49606018+joowoonk@users.noreply.github.com>
2022-05-22 23:15:29 -0500

    f

--- commands/lisa_random.js
+++ commands/lisa_random.js
@@ -2,21 +2,22 @@ const axios = require("axios");
 
 
 module.exports.run = async (bot, message, args) => {
-    let lisa = ["lisa", "lisa%dance", "lisa%cute"];
+    let lisa = ["lalisa", "lisa", "lisa%dance", "lisa%cute"];
     console.log(lisa[Math.floor(Math.random() * lisa.length)]);
-  axios
-    .get(
-      "https://g.tenor.com/v1/random?q=manoban%+" + lisa+ "%blackpink&key=LIVDSRZULELA"
-    )
-    // https://g.tenor.com/v1/random?lisarandom&key=LIVDSRZULELA&limit=1
-    .then((res) => {
-      return message.channel.send(
-        res.data.results[
-          Math.floor(Math.random() * res.data.results.length)
-        ].url
-      );
-    })
-    .catch((er) => console.error(er));
+    axios
+      .get(
+        "https://g.tenor.com/v1/random?q=manoban%+" +
+          lisa +
+          "%blackpink&key=LIVDSRZULELA"
+      )
+      // https://g.tenor.com/v1/random?lisarandom&key=LIVDSRZULELA&limit=1
+      .then((res) => {
+        return message.channel.send(
+          res.data.results[Math.floor(Math.random() * res.data.results.length)]
+            .url
+        );
+      })
+      .catch((er) => console.error(er));
 };
 module.exports.help = {
   name: "lisa",
